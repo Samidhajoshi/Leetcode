c7e86fd7cd2971e789323782547876a76f879284
sammy <samidhajoshi111@gmail.com>
2025-08-04 20:48:07 +0530

    Create majority_element.cpp

---
+++ arrays/majority_element.cpp
@@ -0,0 +1,16 @@
+/*
+This is Day 5 of solving daily leetcode questions.
+Problem 11:Majority Element
+Problem link: https://leetcode.com/problems/majority-element/description/
+
+Approach: If we sort an array the majority element(element with frequency >n/2) will always be at n/2 position.
+*/
+class Solution {
+
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+}
+
+};
