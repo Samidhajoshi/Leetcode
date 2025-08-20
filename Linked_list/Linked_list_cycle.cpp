74ade3629eb293b90025f5652fc63d39517688a1
sammy <samidhajoshi111@gmail.com>
2025-08-20 15:52:37 +0530

    Create Linked_list_cycle.cpp

---
+++ Linked_list/Linked_list_cycle.cpp
@@ -0,0 +1,21 @@
+/*
+This is day 6 of solving daily leetcode questions.
+Problem 11: Linked list cycle
+Problem link -https://leetcode.com/problems/linked-list-cycle/
+
+Approach - Two pointer approach 
+*/
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            if(fast==slow)return 1;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return 0;
+        
+    }
+};
